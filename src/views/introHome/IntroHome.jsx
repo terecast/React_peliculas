c7db6d72b08a34fbededab8e930fa3c56f1c454e
tere <125310682+terecast@users.noreply.github.com>
2023-08-04 00:33:43 -0600

    cambios_4_agosto

--- src/views/introHome/IntroHome.jsx
+++ src/views/introHome/IntroHome.jsx
@@ -6,7 +6,12 @@ export default function IntroHome() {
     <div className="contaner-intro">
         <div className="row">
             <div className="col">
-                <h1>Hey Hey </h1>
+                <h1>BIENVENIDO</h1>
+                <h2>El mundo de peliculas sin salir de casa</h2>
+            </div>
+
+            <div className='movieTime-img'>
+                <img src="https://static.vecteezy.com/system/resources/previews/005/502/524/non_2x/cinema-background-concept-movie-theater-object-on-red-curtain-background-and-movie-time-with-electric-bulbs-frame-illustration-free-vector.jpg" alt="" />
             </div>
         </div>
     </div>
--- src/views/menu/Opciones_menu.jsx
+++ src/views/menu/Opciones_menu.jsx
@@ -8,36 +8,22 @@ function Opciones_menu(){
     
     <header> 
       <nav className="topnav">
-        
 
         <div className='container-nav-title'>
           <span>
-            MOVIE WORLD
+            MOVIE TIME APP
           </span>
-          <img src={cinta} alt="" />
+          
         </div>
 
-        <div className='container-nav-search'>
+        <div className='container-nav-profile'>
+          <span>Teresa Castillo </span>
+          <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
+          
 
-          <div className='container-nav-search-input'>
-            
-            <p>Tu entrada al mundo de las peliculas en tus Manos!</p>
-            
-            
-          </div>
-
-          <div className='container-nav-profile'>
-            <span>Teresa Castillo</span>
-            <img src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHByb2ZpbGV8ZW58MHx8MHx8fDA%3D&w=1000&q=80" alt="" />
-          </div>
-
-
-
+          
         </div>
 
-
-        
-  
       </nav>
 
     </header>
